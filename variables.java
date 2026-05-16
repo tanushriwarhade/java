eb2e6a5e21c388cc72ef4659eac4a8289a03da54
Tanushri Warhade <tanuwarhade@gmail.com>
2026-05-16 23:20:33 +0530

    Remove duplicate main methods and clean up code

--- variables.java
+++ variables.java
@@ -56,3 +56,13 @@ public class variables {
         System.out.println(mul);
     }
 }
+
+public class variables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        int mul = a * b;
+        System.out.println(mul);
+    }
+}
